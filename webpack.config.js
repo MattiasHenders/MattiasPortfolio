a2be89222c8eb7ecb1eb9103bb6ad28bfe14d67f
MattiasHenders <mhenders1@my.bcit.ca>
2022-04-03 01:01:48 -0700

    Updated build

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-module.exports = function (_env, argv) {
-    const isProduction = argv.mode === "production";
-    const isDevelopment = !isProduction;
+module.exports = ({ mode } = { mode: "production" }) => {
+    console.log(`mode is: ${mode}`);
+    const isProduction = ("production" === mode);
 
-    return {
-        devtool: isDevelopment && "cheap-module-source-map",
+    return merge({
+        mode,
         entry: "./src/index.js",
+        devServer: {
+            hot: true,
+            open: true
+        },
         output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "assets/js/[name].[contenthash:8].js",
-            publicPath: "/"
+            publicPath: "/",
+            path: path.resolve(__dirname, "build"),
+            filename: "bundle.js"
         },
         module: {
             rules: [
                 {
-                    test: /\.jsx?$/,
+                    test: /\.(js|jsx)$/,
                     exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            cacheDirectory: true,
-                            cacheCompression: false,
-                            envName: isProduction ? "production" : "development"
-                        }
-                    }
+                    loader: "babel-loader"
                 },
                 {
                     test: /\.css$/,
@@ -39,7 +37,7 @@ module.exports = function (_env, argv) {
                     ]
                 },
                 {
-                    test: /\.(png|jpg|gif)$/i,
+                    test: /\.(png|jpg|gif|pdf)$/i,
                     use: {
                         loader: "url-loader",
                         options: {
@@ -51,7 +49,6 @@ module.exports = function (_env, argv) {
                 {
                     test: /\.svg$/,
                     use: ["@svgr/webpack"]
-
                 },
                 {
                     test: /\.(eot|otf|ttf|woff|woff2)$/,
@@ -59,23 +56,8 @@ module.exports = function (_env, argv) {
                     options: {
                         name: "static/media/[name].[hash:8].[ext]"
                     }
-                },
-                {
-                    test: /\.(pdf)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].[ext]'
-                            }
-                        }
-                    ]
                 }
-
             ]
-        },
-        resolve: {
-            extensions: [".js", ".jsx"]
         },
         plugins: [
             isProduction &&
@@ -83,13 +65,16 @@ module.exports = function (_env, argv) {
                 filename: "assets/css/[name].[contenthash:8].css",
                 chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
             }),
-            new webpack.DefinePlugin({
-                "process.env.NODE_ENV": "production"
-            }),
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, "public/index.html"),
                 inject: true
-            })
+            }),
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(
+                    isProduction ? "production" : "development"
+                )
+            }),
+            new webpack.HotModuleReplacementPlugin()
         ].filter(Boolean),
         optimization: {
             minimize: isProduction,
@@ -110,29 +95,7 @@ module.exports = function (_env, argv) {
                     }
                 }),
                 new OptimizeCssAssetsPlugin()
-            ],
-            splitChunks: {
-                chunks: "all",
-                minSize: 0,
-                maxInitialRequests: 20,
-                maxAsyncRequests: 20,
-                cacheGroups: {
-                    vendors: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name(module, chunks, cacheGroupKey) {
-                            const packageName = module.context.match(
-                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                            )[1];
-                            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-                        }
-                    },
-                    common: {
-                        minChunks: 2,
-                        priority: -10
-                    }
-                }
-            },
-            runtimeChunk: "single"
+            ]
         },
         devServer: {
             compress: true,
@@ -140,5 +103,5 @@ module.exports = function (_env, argv) {
             open: true,
             overlay: true
         }
-    };
-};  
+    });
+};
